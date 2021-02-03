6a39e62875ad652d96380e71afd5e435fa3d9fb6
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-02-04 00:00:49 +0800

    Create script.js

---
+++ Learn/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,19 @@
+
+// Values and Variables
+let country = 'Philippines';
+let continent = 'Asia';
+let population = 20;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types
+let isIsland = true;
+let language;
+console.log(typeof(isIsland));
+console.log(typeof(population));
+console.log(typeof(country));
+console.log(typeof(language));
+
+
+
