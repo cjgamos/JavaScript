947b57e1be3af6efda207afbe9e0a1f7c22f2cad
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-02-05 00:24:52 +0800

    Update script.js

--- Learn/01-Fundamentals-Part-1/script.js
+++ Learn/01-Fundamentals-Part-1/script.js
@@ -72,4 +72,27 @@ if (birthYear <= 2000) {
 console.log(century);
 */
 
+// Type Conversion
+const inputYear = '2000';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
 
+console.log(Number('Carl'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// Type Coercion
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + '23' + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' * '2');
+console.log('23' / '2');
+console.log('23' > '2');
+
+ let n = '1' + 1; // '11'
+ n = n - 1;
+ console.log(n);
+
+ 
