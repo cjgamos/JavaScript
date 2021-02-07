bc3a634ba36187a5db3f06660a5545ee9466a7e6
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-02-07 09:30:40 +0800

    Update script.js

--- Learn/02-Fundamentals-Part-2/script.js
+++ Learn/02-Fundamentals-Part-2/script.js
@@ -1,7 +1,29 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriverLicense = true;
-if (hasDriversLicense) console.log('I can drive');
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive');
+
+// const interface = 'Audio';
+
+function logger() {
+    console.log('My name is Carl');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(3, 4));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
