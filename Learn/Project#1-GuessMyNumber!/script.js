99ee0a720b588422e3c6041666e443dcad888722
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-02-15 00:27:57 +0800

    Update script.js

--- Learn/Project#1-GuessMyNumber!/script.js
+++ Learn/Project#1-GuessMyNumber!/script.js
@@ -10,7 +10,28 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 20;
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-    console.log(document.querySelector('.guess').value);
-})
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'You guess it Correctly!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Too High! Guess Lower!';
+        score--;
+        console.log(document.querySelector('.score').textContent = score);
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Too Low! Guess Higher!';
+        score--;
+        console.log(document.querySelector('.score').textContent = score);
+    } else {
+        document.querySelector('.message').textContent = 'Wrong! Guess again!';
+    }
+});
