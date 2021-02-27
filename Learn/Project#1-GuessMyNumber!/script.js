2527cafa05b4796a715b5c692911ef2bbfbbaee1
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-02-27 15:04:39 +0800

    Update script.js

--- Learn/Project#1-GuessMyNumber!/script.js
+++ Learn/Project#1-GuessMyNumber!/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 30;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', () => {
+    console.log(document.querySelector('.guess').value);
+});
