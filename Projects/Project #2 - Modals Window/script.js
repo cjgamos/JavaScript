9a3973e078e9e508fecafff614677dca3dd4c829
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-02-28 11:02:30 +0800

    Project #2

---
+++ Projects/Project #2 - Modals Window/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+// Opening Modal
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+// Closing Modal
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+// Esc Close Modal
+const keyPressed = (e) => {
+    // console.log(e.key)
+    if (e.key === "Escape" && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', keyPressed);
+
+
