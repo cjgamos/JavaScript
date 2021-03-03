767ca0288aca68d95f328468c5424fd20465bae6
Gamos,CJ C <68680586+cjgamos@users.noreply.github.com>
2021-03-04 02:46:07 +0800

    Update script.js

--- Projects/Project #3 - Pig Game/script.js
+++ Projects/Project #3 - Pig Game/script.js
@@ -17,8 +17,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-let currentScore = 0;
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling Func
 btnRoll.addEventListener('click', () => {
@@ -34,9 +36,12 @@ btnRoll.addEventListener('click', () => {
     if (dice !== 1) {
         // Add dice to current score
         currentScore += dice;
-        current0.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // Next Player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
     }
 });
 
